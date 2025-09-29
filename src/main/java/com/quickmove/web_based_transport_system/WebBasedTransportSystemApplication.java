92e1d7630bdbc9159e7d03cfa05a775b873594ed
YourUsername <YourEmail>
2025-09-29 10:02:00 +0530

    First commit

---
+++ src/main/java/com/quickmove/web_based_transport_system/WebBasedTransportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quickmove.web_based_transport_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedTransportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedTransportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickmove/web_based_transport_system/WebBasedTransportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickmove.web_based_transport_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedTransportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
